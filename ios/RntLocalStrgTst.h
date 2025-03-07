dad8ebf5edf8d3451ac6be5c66941a90d121932b
AllyssonCidade <allyssoncidade@gmail.com>
2025-03-07 08:07:46 -0300

    start library development

--- ios/RntLocalStrgTst.h
+++
@@ -1,6 +0,0 @@
-
-#import "generated/RNRntLocalStrgTstSpec/RNRntLocalStrgTstSpec.h"
-
-@interface RntLocalStrgTst : NSObject <NativeRntLocalStrgTstSpec>
-
-@end
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import RntLocalStrgTst from './NativeRntLocalStrgTst';
-
-export function multiply(a: number, b: number): number {
-  return RntLocalStrgTst.multiply(a, b);
-}
